fc207ed25a6197fc14edd7906372c27321201ee2
Fatim <Fatim@LAPTOP-1MAQ477T>
2022-10-11 22:14:18 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fib_Recursive(11);
+        }
+        public static void Fib_Recursive(int len_)
+        {
+            Fib(0, 1, 1, len_);
+        }
+        private static void Fib(int _a, int _b, int _count, int _len)
+        {
+            if (_count <= _len)
+            {
+                Console.Write("{0}", _a);
+                Fib(_b,_a+_b,_count + 1,_len);
+            }
+        
+        }
+    }
+}
